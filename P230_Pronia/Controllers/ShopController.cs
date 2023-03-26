99f1b1be30f7d1ebf39e7cf7adcec3a61d9f65bc
Jamil-Arifoglu <jamilai@code.edu.az>
2023-03-26 21:17:40 +0400

    Backend task 2

---
+++ P230_Pronia/Controllers/ShopController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P230_Pronia.DAL;
+using P230_Pronia.Entities;
+
+namespace P230_Pronia.Controllers
+{
+	public class ShopController : Controller
+	{
+		readonly ProniaDbContext _context;
+
+        public ShopController(ProniaDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult index()
+        {
+            List<Plant> plants = _context.Plants.
+                  Include(p => p.PlantTags).ThenInclude(tp => tp.Tag).
+                  Include(p => p.PlantCategories).ThenInclude(cp => cp.Category).
+                  Include(p => p.PlantDeliveryInformation).
+                   Include(p => p.PlantImages)
+                  .ToList();
+            return View(plants);
+        }
+        public IActionResult Detail(int id, int categoryId)
+        {
+            Plant? plant = _context.Plants
+                  .Include(p => p.PlantTags)
+                  .ThenInclude(tp => tp.Tag)
+                  .Include(p => p.PlantCategories)
+                  .ThenInclude(cp => cp.Category)
+                  .Include(p => p.PlantDeliveryInformation)
+                  .Include(p => p.PlantImages)
+                  .FirstOrDefault(x => x.Id == id);
+
+            var category = plant?.PlantCategories;
+            if (category is null) return NotFound();
+
+            ViewBag.RelatedPlants = _context.Plants
+                .Include(p => p.PlantImages)
+                .Include(pc => pc.PlantCategories).ThenInclude(c=>c.Category)
+                .Where(p => p.PlantCategories.Any(c => c.CategoryId == categoryId))
+                .ToList();
+
+            return View(plant);
+        }
+      
+
+
+
+
+
+    }
+}
